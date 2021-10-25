74bf5f8551e106f2634eeb3dc22884f7654d41d7
Calbabreaker <calbabreaker@gmail.com>
2021-10-25 12:54:23 +1030

    did more stuf (added sprites)

--- scripts/placement.gd
+++ scripts/placement.gd
@@ -1,15 +1,15 @@
 extends Node2D
 
 var building_prefab := preload("res://scenes/building.tscn")
-var building_grid := Utils.create_grid(20, 30)
-onready var offset := position - Building.SIZE_VECTOR / 2
+var building_grid := Utils.create_grid(15, 18)
+onready var offset: Vector2 = ($"BuildingGrid").position - Building.SIZE_VECTOR / 2
 
 var selected_building := SimpleGenerator
 var antimatter := 0
 
 func _input(event: InputEvent) -> void:
 	if event.is_action_pressed("ui_click"):
-		var offseted_mouse_pos = get_global_mouse_position()
+		var offseted_mouse_pos = get_global_mouse_position() - offset
 		var mouse_cell_pos = (offseted_mouse_pos / Building.SIZE).floor()
 		place_building(mouse_cell_pos)
 		
@@ -23,10 +23,10 @@ func place_building(position: Vector2):
 		building_grid[position.y][position.x] = building_node
 
 func _on_Button_pressed() -> void:
-	Utils.loop_through_grid(building_grid, funcref(self, "building_generator"))
+	Utils.loop_through_grid(building_grid, funcref(self, "building_generate"))
 	
 	var text_label = $"CanvasLayer/AntimatterLabel"
-	text_label.text = "Antimatter %s" % (antimatter)
+	text_label.text = str(antimatter)
 
-func building_generator(building_node, x: int, y: int):
+func building_generate(building_node, x: int, y: int):
 	antimatter += building_node.building.generate()
